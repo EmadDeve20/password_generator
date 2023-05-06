9f7c7339f8d6a87fb761ba7d874831e004860b11
EmadDeve20 <emaddeve20@gmail.com>
2023-05-06 23:27:02 +0330

    use manual args

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use clap::Parser;
 use rand::Rng;
 use std::env;
+use password_generator::Args;
 
 fn main() {
 
-    let args:Vec<String> = env::args().collect();
+    let args = Args::parse();
 
-    let len_password;
+
+    let len_password = args.length;
 
     let characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
     'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
@@ -14,14 +17,7 @@ fn main() {
     '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',
     '=', '+', '`', '\'', '"','|', ';', ':', '>', '<', '.', '/', '?', '\\'];
 
-    if args.len() == 2 {
-        len_password = args[1].parse().unwrap();
-    } else {
-        len_password = 8;
-    }
-
-    
-    
+        
     for _ in 0..len_password {
         let random = rand::thread_rng().gen_range(0..characters.len());
 
