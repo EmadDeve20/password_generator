e2a66ccb697a62634364338c491ca9a8894f2a43
EmadDeve20 <emaddeve20@gmail.com>
2023-05-07 00:04:45 +0330

    create the generator function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use rand::Rng;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -7,4 +8,28 @@ pub struct Args {
     /// Length of Password
     #[arg(short, long, default_value_t = 8)]
     pub length: u8,
+}
+
+fn get_characters() -> Vec<char> {
+    vec!['a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w',
+    'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Z', 'Y', 'Z',
+    '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',
+    '=', '+', '`', '\'', '"','|', ';', ':', '>', '<', '.', '/', '?', '\\']
+}
+
+pub fn generate_password(len: u8) -> String {
+    
+    let characters = get_characters();
+    let mut password = String::new();
+
+
+    for _ in 0..len {
+        let random = rand::thread_rng().gen_range(0..characters.len());
+
+        password = format!("{}{}", password, characters[random]);
+    }
+
+    password
 }
